c3906d513b25b6d82cace1ca6252ee942d91f7fe
Bruno Moraes <brunofloyd@gmail.com>
2016-05-10 22:25:07 -0300

    Correction of the post function on the Server

--- model/user.js
+++ model/user.js
@@ -1,4 +1,6 @@
-var mongoose     = require('mongoose');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/database');
+
 var Schema       = mongoose.Schema;
 
 var UserSchema = new Schema ({
@@ -6,4 +8,4 @@ var UserSchema = new Schema ({
     userPassword : String
 });
 
-module.exports = mongoose.model('users', UserSchema);
+module.exports = mongoose.model('User', UserSchema);
